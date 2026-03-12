0191fa3a519fabd893400e6d77814ed9947d58ef
sayuo4 <sayuo7898@gmail.com>
2026-03-12 16:22:30 +0200

    Fix coyote time

--- src/player/states/air_entry_state.gd
+++ src/player/states/air_entry_state.gd
@@ -2,9 +2,9 @@ class_name AirEntryState
 extends PlayerState
 
 func _enter(previous_state: State) -> void:
+	player.stop_jump_timers()
+	
 	if player.velocity.y >= 0:
 		switch_to("FallState", previous_state)
 	else:
 		switch_to("JumpState", previous_state)
-	
-	player.stop_jump_timers()
